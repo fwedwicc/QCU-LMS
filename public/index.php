c1095b02b6619b930b708987719f046673d967ae
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-02-04 20:16:43 +0800

    Added a login page

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QCU-LMS</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css"  rel="stylesheet" />
+</head>
+<body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+</body>
+</html>
---
+++ src/View/login.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QCU-LMS | Login</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css"  rel="stylesheet" />
+  <link rel="stylesheet" href="login.css">
+</head>
+<body class="bg-gray-900">
+  <div class="parent h-screen">
+    <!-- Left side itey -->
+    <div class="div1 relative">
+      <img src="qcu-image-login.png" alt="" class="object-cover w-full h-full">
+    </div>
+    <!-- Right side itey -->
+    <div class="div2 flex flex-col p-11"> 
+      <span class="inline-flex">
+        <img src="qcu-logo-lms.png" alt="Quezon City University Logo" class="h-9 w-auto mr-6">
+        <h3 class="font-medium text-gray-900 dark:text-white text-2xl mb-5">Learning Management System</h3>
+      </span>
+      <!-- Login Form -->
+      <div class="flex-col flex items-center justify-center h-full">
+        <h1 class="font-bold text-gray-900 dark:text-white text-4xl mb-9">Welcome, QCUians!</h1>
+        <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <form class="space-y-4" action="#">
+            <h5 class="text-xl font-medium text-gray-900 dark:text-white mb-7">Sign in to your account</h5>
+            <!-- Username Input -->
+            <label for="user" class="block text-sm font-medium text-gray-900 dark:text-white">Username</label>
+            <div class="flex">
+              <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
+                </svg>
+              </span>
+              <input type="text" id="user" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your username">
+            </div>
+            <!-- Password Input -->
+            <label for="password" class="block text-sm font-medium text-gray-900 dark:text-white">Password</label>
+            <div class="flex">
+              <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7c0-1.1.9-2 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6c.6 0 1 .4 1 1v3a1 1 0 1 1-2 0v-3c0-.6.4-1 1-1Z" clip-rule="evenodd"/>
+                </svg>
+              </span>
+              <input type="password" id="password" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your password">
+            </div>
+            <!-- Show Password -->
+            <div class="flex items-start">
+              <div class="flex items-start">
+                <div class="flex items-center h-5">
+                  <input id="remember" type="checkbox" onclick="togglePasswordVisibility()" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
+                </div>
+                <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show Password</label>
+              </div>
+              <!-- Optional kemeruts -->
+              <a href="#" class="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+            </div>
+            <!-- Login Button -->
+            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+            <!-- Optional kemeruts -->
+            <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+                Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+            </div>
+          </form>
+        </div>
+        <!-- Footer -->
+        <span id="year" class="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-12"></span>
+      </div>
+    </div>
+  </div>
+  <script>
+    // For password visibility
+    function togglePasswordVisibility() {
+      const passwordInput = document.getElementById('password');
+      passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+    }
+    // Footer date
+    document.addEventListener('DOMContentLoaded', function() {
+      const yearSpan = document.getElementById('year');
+      const currentYear = new Date().getFullYear();
+      yearSpan.innerHTML = `© ${currentYear} <a href="https://qcu.edu.ph/" class="hover:underline" target="_blank">Quezon City University</a>. All Rights Reserved.`;
+    });
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+</body>
+</html>
