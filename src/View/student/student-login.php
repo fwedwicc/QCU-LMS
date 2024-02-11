9217fbf626f18c163f7f46527575df4efb6f1076
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-02-11 14:28:14 +0800

    Fixed the responsiveness of student login page, also added popovers in login links for differect users

--- src/View/student/student-login.php
+++ src/View/student/student-login.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>QCU-Learning Management System | Student Login</title>
   <script src="https://cdn.tailwindcss.com"></script>
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css"  rel="stylesheet" />
-  <link rel="stylesheet" href="../../../public/assets/css/login.css">
-  <link rel="icon" type="image/png" href="../../../public/assets/img/qcu-logo-lms.png"/>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="../../../public/assets/css/scrollbar.css">
+  <link rel="icon" type="image/png" href="../../../public/assets/img/qcu-logo-lms.png" />
 </head>
+
 <body class="bg-gray-900">
-  <div class="parent h-screen">
+  <div class="h-screen grid md:grid-cols-2">
     <!-- Left side itey -->
-    <div class="div1 relative">
-      <img src="../../../public/assets/img/qcu-image-login.png" class="object-cover w-full h-full" alt="QCU Photography by Joanah my bb" >
+    <div class="col-span-1 hidden md:block">
+      <img src="../../../public/assets/img/qcu-image-login.png" class="object-cover w-full h-full" alt="QCU Photography by Joanah my bb">
     </div>
     <!-- Right side itey -->
-    <div class="div2 flex flex-col p-11"> 
+    <div class="col-span-1 p-12">
       <span class="inline-flex">
         <img src="../../../public/assets/img/qcu-logo-lms.png" alt="Quezon City University Logo" class="h-9 w-auto mr-6">
-        <h3 class="font-medium text-gray-900 dark:text-white text-2xl mb-5">Learning Management System</h3>
+        <h3 class="font-medium text-gray-900 dark:text-white text-2xl mb-5">QCU-LMS</h3>
       </span>
-      <!-- Login Form -->
+      <!-- Greeting -->
       <div class="flex-col flex items-center justify-center h-full">
-        <h1 class="font-bold text-gray-900 dark:text-white text-4xl mb-9">Welcome, QCUians!</h1>
+        <h1 class="font-bold text-gray-900 dark:text-white text-4xl mb-8">Welcome, QCUians!</h1>
         <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <!-- Form -->
           <form class="space-y-4" action="#">
             <h5 class="text-xl font-medium text-gray-900 dark:text-white mb-7">Sign in to your account</h5>
             <!-- Username Input -->
@@ -32,7 +35,7 @@
             <div class="flex">
               <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                 <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                  <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
+                  <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd" />
                 </svg>
               </span>
               <input type="text" id="user" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your username">
@@ -42,7 +45,7 @@
             <div class="flex">
               <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                 <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                  <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7c0-1.1.9-2 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6c.6 0 1 .4 1 1v3a1 1 0 1 1-2 0v-3c0-.6.4-1 1-1Z" clip-rule="evenodd"/>
+                  <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7c0-1.1.9-2 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6c.6 0 1 .4 1 1v3a1 1 0 1 1-2 0v-3c0-.6.4-1 1-1Z" clip-rule="evenodd" />
                 </svg>
               </span>
               <input type="password" id="password" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your password">
@@ -60,17 +63,58 @@
             </div>
             <!-- Login Button -->
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-            <!-- Optional kemeruts -->
+            <!-- Login link kemeruts -->
             <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+              Are you <a href="#" data-popover-target="admin-popover" data-popover-placement="bottom" class="text-blue-700 hover:underline dark:text-blue-500">Admin</a> or <a href="#" data-popover-target="instructor-popover" data-popover-placement="bottom" class="text-blue-700 hover:underline dark:text-blue-500">Instructor</a>?
+            </div>
+            <!-- Popver for admin login link -->
+            <div data-popover id="admin-popover" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+              <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 class="font-semibold text-gray-900 dark:text-white">Admin login page</h3>
+              </div>
+              <div class="px-3 py-2">
+                <p>Click this link and you'll be directed to the admin login page.</p>
+              </div>
+              <div data-popper-arrow></div>
+            </div>
+            <!-- Popver for instructor login link -->
+            <div data-popover id="instructor-popover" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+              <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 class="font-semibold text-gray-900 dark:text-white">Instructor login page</h3>
+              </div>
+              <div class="px-3 py-2">
+                <p>Click this link and you'll be directed to the instructor login page.</p>
+              </div>
+              <div data-popper-arrow></div>
             </div>
           </form>
         </div>
         <!-- Footer -->
-        <span id="year" class="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-12"></span>
+        <span id="year" class="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-9"></span>
+        <!-- Popover for QCU Website footer -->
+        <div data-popover id="qcu-popover" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-96 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
+          <div class="grid grid-cols-5">
+            <div class="col-span-3 p-3">
+              <div class="space-y-2">
+                <h3 class="font-semibold text-gray-900 dark:text-white">About Quezon City University</h3>
+                <p>Italy is located in the middle of the Mediterranean Sea, in Southern Europe it is also considered part of Western Europe. A unitary parliamentary republic with Rome as its capital and largest city.</p>
+                <a href="https://qcu.edu.ph/" target="_blank" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:underline">Read more <svg class="w-2 h-2 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg></a>
+              </div>
+            </div>
+            <img src="/docs/images/popovers/italy.png" class="h-full col-span-2" alt="Italy map" />
+          </div>
+          <div data-popper-arrow></div>
+        </div>
       </div>
     </div>
   </div>
+  <style>
+    body::-webkit-scrollbar {
+      width: 0;
+    }
+  </style>
   <script>
     // For password visibility
     function togglePasswordVisibility() {
@@ -81,9 +125,10 @@
     document.addEventListener('DOMContentLoaded', function() {
       const yearSpan = document.getElementById('year');
       const currentYear = new Date().getFullYear();
-      yearSpan.innerHTML = `© ${currentYear} <a href="https://qcu.edu.ph/" class="hover:underline" target="_blank">Quezon City University</a>. All Rights Reserved.`;
+      yearSpan.innerHTML = `© ${currentYear} <a href="https://qcu.edu.ph/" data-popover-target="qcu-popover" class="hover:underline" target="_blank">Quezon City University</a>. All Rights Reserved.`;
     });
   </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 </body>
+
 </html>
