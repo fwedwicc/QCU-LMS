2614a3be9a93a505ae7fcecde49ac4ee3ecfc365
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-02-12 11:18:55 +0800

    Finished all login forms, and successfully linked all their anchors

--- src/View/instructor/instructor-login.php
+++ src/View/instructor/instructor-login.php
@@ -61,7 +61,7 @@
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
             <!-- Login link kemeruts -->
             <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-              Are you <a href="../student/student-login.php" data-popover-target="student-popover" data-popover-placement="bottom" class="text-blue-700 hover:underline dark:text-blue-500">Student</a> or <a href="#" data-popover-target="admin-popover" data-popover-placement="bottom" class="text-blue-700 hover:underline dark:text-blue-500">Admin</a>?
+              Are you <a href="../student/student-login.php" data-popover-target="student-popover" data-popover-placement="bottom" class="text-blue-700 hover:underline dark:text-blue-500">Student</a> or <a href="../admin/admin-login.php" data-popover-target="admin-popover" data-popover-placement="bottom" class="text-blue-700 hover:underline dark:text-blue-500">Admin</a>?
             </div>
             <!-- Popver for admin login link -->
             <div data-popover id="admin-popover" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
