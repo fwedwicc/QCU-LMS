a8487bc077a8a8826706cc24fd14833b2cbeb713
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-02-13 17:17:45 +0800

    Created student template for main side and navigation bar

--- src/View/student/student.php
+++ src/View/student/student.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>QCU-Learning Management System | Admin</title>
+  <title>QCU-Learning Management System | Student</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="../../../public/assets/css/scrollbar.css">
-  <link rel="icon" type="image/png" href="../../../public/assets/img/qcu-logo-lms.png"/>
+  <link rel="icon" type="image/png" href="../../../public/assets/img/qcu-logo-lms.png" />
 </head>
 
 <body class="bg-gray-900">
@@ -171,77 +171,145 @@
             <span class="ms-3">Dashboard</span>
           </a>
         </li>
-        <li>
-          <!-- Link -->
-          <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-            </svg>
-            <span class="flex-1 ms-3 whitespace-nowrap">Inbox</span>
-            <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">0</span>
-          </a>
-        </li>
         <!-- Course dropdown -->
         <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-programs">
           <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2c.6 0 1-.4 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z" clip-rule="evenodd" />
           </svg>
-          <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Programs</span>
+          <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Courses</span>
           <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
           </svg>
         </button>
         <ul id="dropdown-programs" class="hidden py-2 space-y-2">
           <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSENT</a>
+            <a href="#" data-popover-target="first-subject" data-popover-placement="right" type="button" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">SE101</a>
+            <!-- Course overview popover -->
+            <div data-popover id="first-subject" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+              <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 class="font-semibold text-gray-900 dark:text-white">Title: Software Engineering</h3>
+              </div>
+              <div class="px-3 py-2 space-y-2">
+                <span class="block">
+                  <span class="text-white">Professor:</span>
+                  <span>Mary Ann Manandeg</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Section:</span>
+                  <span>SBIT-2A</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Description:</span>
+                  <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptas neque molestiae quibusdam aspernatur esse libero magnam mollitia quod, consequuntur facere soluta obcaecati eveniet voluptatem cumque.</span>
+                </span>
+                <a href="" target="_blank" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:underline">View Subject <svg class="w-2 h-2 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg></a>
+              </div>
+              <div data-popper-arrow></div>
+            </div>
           </li>
           <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSA</a>
+            <a href="#" data-popover-target="second-subject" data-popover-placement="right" type="button" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">IPT101</a>
+            <!-- Course overview popover -->
+            <div data-popover id="second-subject" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+              <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 class="font-semibold text-gray-900 dark:text-white">Title: Integrative Programming and Technologies 1</h3>
+              </div>
+              <div class="px-3 py-2 space-y-2">
+                <span class="block">
+                  <span class="text-white">Professor:</span>
+                  <span>Chrisitan Escoto</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Section:</span>
+                  <span>SBIT-2A</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Description:</span>
+                  <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptas neque molestiae quibusdam aspernatur esse libero magnam mollitia quod, consequuntur facere soluta obcaecati eveniet voluptatem cumque.</span>
+                </span>
+                <a href="" target="_blank" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:underline">View Subject <svg class="w-2 h-2 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg></a>
+              </div>
+              <div data-popper-arrow></div>
+            </div>
           </li>
           <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSMA</a>
+            <a href="#" data-popover-target="third-subject" data-popover-placement="right" type="button" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">IM101</a>
+            <!-- Course overview popover -->
+            <div data-popover id="third-subject" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+              <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 class="font-semibold text-gray-900 dark:text-white">Title: Advance Database Systems</h3>
+              </div>
+              <div class="px-3 py-2 space-y-2">
+                <span class="block">
+                  <span class="text-white">Professor:</span>
+                  <span>Zharina Pascual</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Section:</span>
+                  <span>SBIT-2A</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Description:</span>
+                  <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptas neque molestiae quibusdam aspernatur esse libero magnam mollitia quod, consequuntur facere soluta obcaecati eveniet voluptatem cumque.</span>
+                </span>
+                <a href="" target="_blank" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:underline">View Subject <svg class="w-2 h-2 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg></a>
+              </div>
+              <div data-popper-arrow></div>
+            </div>
           </li>
           <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSIE</a>
+            <a href="#" data-popover-target="fourth-subject" data-popover-placement="right" type="button" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">MS102</a>
+            <!-- Course overview popover -->
+            <div data-popover id="fourth-subject" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+              <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 class="font-semibold text-gray-900 dark:text-white">Title: Quantitative Methods</h3>
+              </div>
+              <div class="px-3 py-2 space-y-2">
+                <span class="block">
+                  <span class="text-white">Professor:</span>
+                  <span>Luisito Soriano</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Section:</span>
+                  <span>SBIT-3G</span>
+                </span>
+                <span class="block">
+                  <span class="text-white">Description:</span>
+                  <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptas neque molestiae quibusdam aspernatur esse libero magnam mollitia quod, consequuntur facere soluta obcaecati eveniet voluptatem cumque.</span>
+                </span>
+                <a href="" target="_blank" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:underline">View Subject <svg class="w-2 h-2 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg></a>
+              </div>
+              <div data-popper-arrow></div>
+            </div>
           </li>
           <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSECE</a>
+            <!-- Link -->
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M9.6 2.6A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2l.5.3a2 2 0 0 1 2.9 0l1.4 1.3a2 2 0 0 1 0 2.9l.1.5h.1a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2l-.3.5a2 2 0 0 1 0 2.9l-1.3 1.4a2 2 0 0 1-2.9 0l-.5.1v.1a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2l-.5-.3a2 2 0 0 1-2.9 0l-1.4-1.3a2 2 0 0 1 0-2.9l-.1-.5H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2l.3-.5a2 2 0 0 1 0-2.9l1.3-1.4a2 2 0 0 1 2.9 0l.5-.1V4c0-.5.2-1 .6-1.4ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd" />
+              </svg>
+              <span class="ms-3">Settings</span>
+            </a>
           </li>
           <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSIT</a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSCS</a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSIS</a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">BSCE</a>
-          </li>
-        </ul>
-        <!-- Users dropdown -->
-        <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-users">
-          <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-          </svg>
-          <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Users</span>
-          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-          </svg>
-        </button>
-        <ul id="dropdown-users" class="hidden py-2 space-y-2">
-          <li>
-            <a href="admin-user-student.php" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Student</a>
-          </li>
-          <li>
-            <a href="admin-user-instructor.php" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Instructor</a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Admin</a>
-          </li>
-        </ul>
+            <!-- Link -->
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2 1 1 0 1 0 0-2Z" clip-rule="evenodd" />
+              </svg>
 
+              <span class="ms-3">Help</span>
+            </a>
+          </li>
+        </ul>
       </ul>
     </div>
   </aside>
