35f8e2199613c79c122948be16d5501e4d3e4c85
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-02-18 14:42:40 +0800

    Finalized the draft in landing/front page

--- src/View/landing-page.php
+++ src/View/landing-page.php
@@ -18,9 +18,10 @@
       <span class="inline-flex mb-11">
         <img src="../../public/assets/img/qcu-logo-lms.png" alt="Quezon City University Logo" class="h-24 w-auto mr-4">
       </span>
-      <h1 class="font-bold text-gray-900 dark:text-white lg:text-5xl text-2xl mb-8">Quezon City University</h1>
+      <h1 class="font-bold text-gray-900 dark:text-white lg:text-5xl text-2xl mb-4">Quezon City University</h1>
+      <h3 class="font-medium text-gray-900 dark:text-white lg:text-2xl text-lg mb-8">Learning Management System</h3>
       <!-- Links -->
-      <div class="inline-flex rounded-md shadow-lg mt-12">
+      <div class="inline-flex rounded-md shadow-md mt-12">
         <a href="../View/student/student-login.php" class="lg:px-10 lg:py-4 lg:text-base px-6 py-3 text-sm font-medium text-blue-700 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
           Student
         </a>
@@ -33,23 +34,23 @@
       </div>
       <!-- Footer -->
       <div class="mt-14 space-x-2">
-        <a href="">
+        <a href="https://web.facebook.com/qcu1994" target="_blank">
           <svg class="inline-block w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M13.1 6H15V3h-1.9A4.1 4.1 0 0 0 9 7.1V9H7v3h2v10h3V12h2l.6-3H12V6.6a.6.6 0 0 1 .6-.6h.5Z" clip-rule="evenodd" />
           </svg>
         </a>
-        <a href="">
+        <a href="https://twitter.com/qcublesds?lang=en" target="_blank">
           <svg class="inline-block w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <path fill="currentColor" d="M13.8 10.5 20.7 2h-3l-5.3 6.5L7.7 2H1l7.8 11-7.3 9h3l5.7-7 5.1 7H22l-8.2-11.5Zm-2.4 3-1.4-2-5.6-7.9h2.3l4.5 6.3 1.4 2 6 8.5h-2.3l-4.9-7Z" />
           </svg>
         </a>
-        <a href="">
-          <svg class="inline-block w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-            <path fill-rule="evenodd" d="M12 22a10 10 0 0 1-7.1-3A9.9 9.9 0 0 1 5 4.8C7 3 9.5 2 12.2 2h.2c2.4 0 4.8 1 6.6 2.6l-2.5 2.3a6.2 6.2 0 0 0-4.2-1.6c-1.8 0-3.5.7-4.8 2a6.6 6.6 0 0 0-.1 9.3c1.2 1.3 2.9 2 4.7 2h.1a6 6 0 0 0 4-1.1c1-.9 1.8-2 2.1-3.4v-.2h-6v-3.4h9.6l.1 1.9c-.1 5.7-4 9.6-9.7 9.6H12Z" clip-rule="evenodd" />
+        <a href="https://www.youtube.com/@qcucss8272" target="_blank">
+          <svg class="inline-block w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M21.7 8c0-.7-.4-1.3-.8-2-.5-.5-1.2-.8-2-.8C16.2 5 12 5 12 5s-4.2 0-7 .2c-.7 0-1.4.3-2 .9-.3.6-.6 1.2-.7 2l-.2 3.1v1.5c0 1.1 0 2.2.2 3.3 0 .7.4 1.3.8 2 .6.5 1.4.8 2.2.8l6.7.2s4.2 0 7-.2c.7 0 1.4-.3 2-.9.3-.5.6-1.2.7-2l.2-3.1v-1.6c0-1 0-2.1-.2-3.2ZM10 14.6V9l5.4 2.8-5.4 2.8Z" clip-rule="evenodd" />
           </svg>
         </a>
       </div>
-      <span id="year" class="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-5"></span>
+      <span id="year" class="block text-xs lg:text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-5"></span>
       <!-- Popover for QCU Website footer -->
       <div data-popover id="qcu-popover" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-96 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
         <div class="grid grid-cols-5">
