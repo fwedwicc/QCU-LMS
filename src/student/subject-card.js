42d5478266d4a2cebebab715efbb4cd8ba22ea4d
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-05-27 18:11:09 +0800

    Finished the styles for dark mode of subject cards in student dashboard

--- src/student/subject-card.js
+++ src/student/subject-card.js
@@ -1,18 +1,17 @@
 // Function to create course card
 function createCourseCard(course) {
   return `
-        <!-- Course Card -->
-        <a href="${course.link}" class="relative block p-6 bg-white rounded-2xl hover:shadow-[15px_15px_30px_-3px_rgba(224,227,246)] transition duration-700 ease-in-out col-span-1 overflow-y-auto border-b-4 border-transparent hover:border-neon-blue">
+        <a href="${course.link}" class="subject-container">
           <div class="relative">
-            <img src="../../assets/image/course-card-illustration.png" class="rounded-xl w-full" alt="Course Card Illustration" draggable="false">
-            <h5 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mb-3 text-4xl font-bold tracking-tight text-white dark:text-white">
+            <img src="../../assets/image/course-card-illustration.png" class="rounded-xl w-full" alt="Course Card Illustration">
+            <h5 class="subject-code">
               ${course.code}</h5>
           </div>
           <h5 class="mt-7 text-2xl font-medium tracking-tight text-corn-flower-blue">${course.title}</h5>
-          <span class="block text-deep-koamaru">${course.instructor}</span>
-          <span class="block text-deep-koamaru">${course.section}</span>
+          <span class="block text-deep-koamaru dark:text-dark-corn-koamaru">${course.instructor}</span>
+          <span class="block text-deep-koamaru dark:text-dark-corn-koamaru">${course.section}</span>
           <!-- Semester Indicator -->
-          <span class="mt-4 inline-flex items-center bg-ghost-lavender text-neon-blue text-sm font-medium px-4 py-2 rounded-full">
+          <span class="subject-sem">
             ${course.semester}
           </span>
         </a>
