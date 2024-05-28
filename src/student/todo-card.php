f3dad883b584f7703903b42b465d444d89826d88
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-05-29 00:37:20 +0800

    Modified the styles of to-do card

--- src/student/todo-card.php
+++ src/student/todo-card.php
@@ -1,29 +1,29 @@
-<!-- Todo card -->
-<div class="block max-w-sm p-6 bg-white rounded-2xl drop-shadows row-span-1 overflow-y-auto">
-<h5 class="mb-4 text-2xl font-bold tracking-tight text-corn-flower-blue">To-do</h5>
+<!-- FIXME: To-do card -->
+<div class="container">
+<h5 class="mb-4 text-2xl font-bold">To-do</h5>
 <div class="max-w-md space-y-2.5 text-deep-koamaru list-inside">
   <a href="#"
-    class="items-center bg-ghost-white px-4 py-2.5 rounded-lg flex justify-between hover:bg-ghost-lavender group transition duration-300 ease-in-out hover:text-neon-blue">
+    class="todo-link group">
     <span class="text-sm font-medium">SE101 - Lab Activity 1</span>
-    <svg class="w-4 h-4 text-deep-koamaru hover:text-neon-blue" aria-hidden="true"
+    <svg class="w-4 h-4 text-deep-koamaru dark:text-dark-corn-koamaru group-hover:text-neon-blue" aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         d="m9 5 7 7-7 7" />
     </svg>
   </a>
   <a href="#"
-    class="items-center bg-ghost-white px-4 py-2.5 rounded-lg flex justify-between hover:bg-ghost-lavender group transition duration-300 ease-in-out hover:text-neon-blue">
+    class="todo-link group">
     <span class="text-sm font-medium">SE101 - Lab Activity 2</span>
-    <svg class="w-4 h-4 text-deep-koamaru hover:text-neon-blue" aria-hidden="true"
+    <svg class="w-4 h-4 text-deep-koamaru dark:text-dark-corn-koamaru group-hover:text-neon-blue" aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         d="m9 5 7 7-7 7" />
     </svg>
   </a>
   <a href="#"
-    class="items-center bg-ghost-white px-4 py-2.5 rounded-lg flex justify-between hover:bg-ghost-lavender group transition duration-300 ease-in-out hover:text-neon-blue">
+    class="todo-link group">
     <span class="text-sm font-medium">IPT101 - Lab Activity 1</span>
-    <svg class="w-4 h-4 text-deep-koamaru hover:text-neon-blue" aria-hidden="true"
+    <svg class="w-4 h-4 text-deep-koamaru dark:text-dark-corn-koamaru group-hover:text-neon-blue" aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         d="m9 5 7 7-7 7" />
